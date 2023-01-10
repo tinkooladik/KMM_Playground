2333540f8d3a883314615ddc0224351069d1baa6
oleksandra <tinkooladik@gmail.com>
2023-01-10 11:57:19 -0500

    initial commit

---
+++ shared/src/commonMain/kotlin/com/tinkooladik/kmmplayground/Greeting.kt
@@ -0,0 +1,9 @@
+package com.tinkooladik.kmmplayground
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greeting(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
