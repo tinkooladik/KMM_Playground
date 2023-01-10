df8637a7152db582c7c921e9a3ef2ffbbbcc9a61
oleksandra <tinkooladik@gmail.com>
2023-01-10 12:43:48 -0500

    added some dependencies

--- shared/src/commonMain/kotlin/com/tinkooladik/kmmplayground/Greeting.kt
+++ shared/src/commonMain/kotlin/com/tinkooladik/kmmplayground/Greeting.kt
@@ -4,6 +4,7 @@ class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greeting(): String {
-        return "Hello, ${platform.name}!"
+        return "Guess what it is! > ${platform.name.reversed()}!" +
+                "\nThere are only ${daysUntilNewYear()} days left until New Year! 🎆"
     }
 }
